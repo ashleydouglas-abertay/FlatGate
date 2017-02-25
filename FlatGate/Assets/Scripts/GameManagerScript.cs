38c3745c52e50bdb02bf1821b796fe2f692935e1
unknown <1503347@MC8163.uad.ac.uk>
2017-02-25 14:10:37 +0000

    Player GoTo Working Mouse Pos Detection WIP

---
+++ FlatGate/Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    // Variables
+    public GameObject player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        // Detect click for movement
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            player.GetComponent<PlayerGoToScript>().SetTargetPos(mousePos);
+            Debug.Log("Mouse Triggered");
+        }
+    }
+}
---
+++ FlatGate/Assets/Scripts/PlayerGoToScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGoToScript : MonoBehaviour
+{
+    // Variables
+    public Vector3 startPos;
+    public float moveSpeed;
+
+    private Vector3 moveVelo = new Vector3(0, 0, 0);
+    private float momentum = 0;
+
+    public Vector3 ForcedTargetPos;
+    private Vector3 currPos;
+    private Vector3 targetPos;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        // DEBUGINFO
+        targetPos = ForcedTargetPos;
+        // END
+
+        // Set current position
+        currPos = transform.position;
+
+        // Checks to see if current position is at target
+        if (currPos.x <= targetPos.x - 0.5 || currPos.x >= targetPos.x + 0.5)
+        {
+            // Work out movement needed
+            moveVelo.x = targetPos.x - currPos.x;
+            // Normalize movement
+            moveVelo.Normalize();
+
+            // Calculate momentum
+            if (momentum == 0)
+            {
+                momentum = 0.1f;
+            }
+            else
+            {
+                if (momentum < 0.75f)
+                {
+                    momentum = momentum * 1.25f;
+                }
+                else
+                {
+                    momentum = 1.0f;
+                }
+            }
+
+            // Apply movement
+            transform.position += (moveVelo * moveSpeed * momentum * Time.deltaTime);
+        }
+        else
+        {
+            // Reset momentum
+            momentum = 0;
+        }
+	}
+
+    // Sets the players target position
+    public void SetTargetPos(Vector3 newTargetPos)
+    {
+        targetPos = newTargetPos;
+        Debug.Log("Target Changed");
+    }
+}
